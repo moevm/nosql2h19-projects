84a1b8fc41a761f79472a9bc631b70b8f918c1c7
Дарина Герасимова <ger-darina@yandex.ru>
2019-12-11 22:18:22 +0300

    update participant

---
+++ frontend/src/colors.js
@@ -0,0 +1,27 @@
+export const COLORS = [
+    {
+        backgroundColor: 'rgba(213,62,79,1.0)',
+        borderColor: 'rgba(213,62,79,0.8)',
+        borderWidth: 1,
+    },
+    {
+        backgroundColor: 'rgba(254,224,139,1.0)',
+        borderColor: 'rgba(254,224,139,0.8)',
+        borderWidth: 1,
+    },
+    {
+        backgroundColor: 'rgba(171,221,164,1.0)',
+        borderColor: 'rgba(171,221,164,0.8)',
+        borderWidth: 1,
+    },
+    {
+        backgroundColor: 'rgba(55,182,155,1.0)',
+        borderColor: 'rgba(55,182,155,0.8)',
+        borderWidth: 1,
+    },
+    {
+        backgroundColor: 'rgba(244,109,67,1.0)',
+        borderColor: 'rgba(244,109,67,0.8)',
+        borderWidth: 1,
+    },
+];
